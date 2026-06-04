c43228a21f1d61985e13abddb44483985b5615e7
Victor Gabriel Passos de Souza <victopassos@uni9.edu.br>
2026-06-04 15:19:51 -0300

    ajuste github pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/projeto-nexthire">
+    <BrowserRouter basename="/Projeto-Nexthire">
       <App />
     </BrowserRouter>
   </React.StrictMode>
